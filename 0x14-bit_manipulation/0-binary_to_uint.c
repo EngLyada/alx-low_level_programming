8a41097eab376124a31987192798deb9d9246140
EngLyada <lyadaemmanuel@gmail.com>
2023-09-01 14:23:13 -0700

    firsr

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,20 +5,20 @@
  * @c: string containing the binary number
  *
  * Return: the converted number c_val
- */
+*/
 
 unsigned int binary_to_uint(const char *c)
 {
-   int e;
-   unsigned int c_val = 0;
-  
-  if (!c)
-      return (0);
-  for (e = 0; c[e]; e++)
-  {
-      if (c[e] < '0' || c[e] > '1')
-        return (0);
-      c_val = 2 * c_val + (c[e] - '0');
-  }
-    return (c_val);
+	int e;
+	unsigned int c_val = 0;
+
+	if (!c)
+		return (0);
+	for (e = 0; c[e]; e++)
+	{
+		if (c[e] < '0' || c[e] > '1')
+			return (0);
+		c_val = 2 * c_val + (c[e] - '0');
+	}
+	return (c_val);
 }
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,28 +1,24 @@
 #include "main.h"
 
 /**
-* The binary of a decimal number is printed by
- * The void method print_binary 
- * k is the number to print in binary
- */
+* print_binary - prints the binary  of a decimal number
+* @k: number to print in binary
+*/
 
 void print_binary(unsigned long int k)
 {
-  int d, count = 0;
-  unsigned long int z;
+	int d, count = 0;
+	unsigned long int z;
+	for (d = 63; d >= 0; d--)
+	{
+		z = k >> d;
+		if (z & 1)
+		{
+			_putchar('1');
+			count++;
+		}else if (count)
+			_putchar('0');
+	} if (!count)
+	 _putchar('0');
 
-  for (d = 63; d >= 0; d--)
-  {
-    z = k >> d;
-
-    if (z & 1)
-    {
-      _putchar('1');
-      count++;
-    }
-    else if (count)
-      _putchar('0');
-  }
-  if (!count)
-    _putchar('0');
 }
