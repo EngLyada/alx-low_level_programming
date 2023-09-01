51cc168f20e7d06a672c044ecedcb7cbd008c2ef
Lyada Emmanuel <lyadaemmanuel@gmail.com>
2023-09-01 23:25:14 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,15 +9,16 @@
 
 unsigned int binary_to_uint(const char *c)
 {
-  int e;
-  unsigned int c_val = 0;
+   int e;
+   unsigned int c_val = 0;
+  
   if (!c)
-    return (0);
+      return (0);
   for (e = 0; c[e]; e++)
   {
-    if (c[e] < '0' || c[e] > '1')
-      return (0);
-    c_val = 2 * c_val + (c[e] - '0');
+      if (c[e] < '0' || c[e] > '1')
+        return (0);
+      c_val = 2 * c_val + (c[e] - '0');
   }
-  return (c_val);
+    return (c_val);
 }
